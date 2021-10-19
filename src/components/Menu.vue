639908193e307b5265cca3fdacb3dcc8dec74e9f
Carlos <contact@carloscolin.me>
2021-10-18 23:50:58 -0500

    :tada: Initial commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+          <span class="navbar-brand">Tweet simulator</span>
+      </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
