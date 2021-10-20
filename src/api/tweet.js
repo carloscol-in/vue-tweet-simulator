9cd86f9567f7bf25d2282fe324dfc13eca075ac4
Carlos <contact@carloscolin.me>
2021-10-19 21:53:20 -0500

    added delete tweet function

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -1,13 +1,32 @@
 import { v4 as uuidv4 } from "uuid"
+import { remove } from "lodash"
 import { TWEETS } from "../utils/constants"
 
-export function saveTweetApi(tweet_message, username) {
+export function saveTweetApi(message, username) {
+  const tweets = getTweetsApi()
   const tweet = {
     id: uuidv4(),
-    tweet_message,
+    message,
     username,
     createdAt: new Date(),
   }
-  const tweet_txt = JSON.stringify(tweet)
-  localStorage.setItem(TWEETS, tweet_txt)
+  tweets.push(tweet)
+  const tweets_txt = JSON.stringify(tweets)
+  localStorage.setItem(TWEETS, tweets_txt)
+}
+
+export function getTweetsApi() {
+  const tweets = localStorage.getItem(TWEETS)
+  if (tweets) return JSON.parse(tweets)
+  return []
+}
+
+export function deleteTweetApi(id) {
+  const tweets = getTweetsApi()
+
+  remove(tweets, function (tweet) {
+    return tweet.id === id
+  })
+
+  localStorage.setItem(TWEETS, JSON.stringify(tweets))
 }
