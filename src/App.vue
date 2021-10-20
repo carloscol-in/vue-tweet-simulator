0702571908ae9ec939fcca866eae76e1d4cfb0e4
Carlos <contact@carloscolin.me>
2021-10-19 21:52:50 -0500

    load and reload tweets functionality

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <Menu :openCloseForm="openCloseForm" :showForm="showForm" />
 
-  <TweetForm :showForm="showForm" />
+  <TweetForm :showForm="showForm" :reloadTweets="reloadTweets" :openCloseForm="openCloseForm" />
+
+  <TweetList :tweets="tweets" :reloadTweets="reloadTweets" />
 </template>
 
 <script>
+import { ref } from "vue"
 import Menu from "./components/Menu.vue"
 import TweetForm from "./components/TweetForm.vue"
+import TweetList from "./components/TweetList.vue"
 import useFormTweet from "./hooks/useFormTweet"
+import { getTweetsApi } from "./api/tweet"
 
 export default {
   name: "App",
   components: {
     Menu,
     TweetForm,
+    TweetList,
   },
   setup() {
+    const loadTweets = () => {
+      return getTweetsApi().reverse()
+    }
+
+    let tweets = ref(loadTweets())
+
+    const reloadTweets = () => {
+      tweets.value = loadTweets()
+    }
+
     return {
       ...useFormTweet(),
+      tweets,
+      reloadTweets,
     }
   },
 }
